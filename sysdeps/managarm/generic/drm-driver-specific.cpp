8d62f4c5c19af54b1faee4ec8b50ea47a506fe15
no92 <no92.mail@gmail.com>
2023-04-01 00:52:45 +0200

    sysdeps/managarm: implement virtio-gpu resource create ioctl

--- sysdeps/managarm/generic/drm-driver-specific.cpp
+++ sysdeps/managarm/generic/drm-driver-specific.cpp
@@ -71,6 +71,45 @@ int ioctl_drm_virtio(int fd, unsigned long request, void *arg, int *result, HelH
 			*reinterpret_cast<uintptr_t *>(param->value) = resp.value();
 			break;
 		}
+		case DRM_VIRTGPU_RESOURCE_CREATE: {
+			auto param = reinterpret_cast<drm_virtgpu_resource_create *>(arg);
+			managarm::fs::DrmIoctlVirtioCreateResourceRequest<MemoryAllocator> req{getSysdepsAllocator()};
+			req.set_target(param->target);
+			req.set_format(param->format);
+			req.set_bind(param->bind);
+			req.set_width(param->width);
+			req.set_height(param->height);
+			req.set_depth(param->depth);
+			req.set_array_size(param->array_size);
+			req.set_last_level(param->last_level);
+			req.set_nr_samples(param->nr_samples);
+			req.set_flags(param->flags);
+			req.set_size(param->size);
+			req.set_stride(param->stride);
+
+			auto [offer, send_ioctl_req, send_ioctl_driver_req, send_req, recv_resp] =
+			exchangeMsgsSync(
+				handle,
+				helix_ng::offer(
+					helix_ng::sendBragiHeadOnly(ioctl_req, getSysdepsAllocator()),
+					helix_ng::sendBragiHeadOnly(ioctl_driver_req, getSysdepsAllocator()),
+					helix_ng::sendBragiHeadOnly(req, getSysdepsAllocator()),
+					helix_ng::recvInline()
+			));
+			HEL_CHECK(offer.error());
+			HEL_CHECK(send_ioctl_req.error());
+			HEL_CHECK(send_ioctl_driver_req.error());
+			HEL_CHECK(send_req.error());
+			HEL_CHECK(recv_resp.error());
+
+			managarm::fs::DrmIoctlVirtioCreateResourceReply<MemoryAllocator> resp{getSysdepsAllocator()};
+			resp.ParseFromArray(recv_resp.data(), recv_resp.length());
+
+			param->bo_handle = resp.bo_handle();
+			param->res_handle = resp.res_handle();
+
+			break;
+		}
 		case DRM_VIRTGPU_GET_CAPS: {
 			auto param = reinterpret_cast<drm_virtgpu_get_caps *>(arg);
 			managarm::fs::DrmIoctlVirtioGetCapsRequest<MemoryAllocator> req{getSysdepsAllocator()};
